55c77237f74ae19f09facbe3b45156530534523f
Maxterious <teichmann.max@gmx.de>
2023-10-15 23:32:46 +0200

    changed layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
-import { Analytics } from '@vercel/analytics/react'
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Image from "next/image";
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,9 +19,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <div>
+          <Image
+            className="-z-10"
+            src={"/background.svg"}
+            alt="Background image"
+            style={{ objectFit: "cover" }}
+            fill={true}
+          />
+        </div>
         {children}
-        <Analytics/>
-        </body>
+        <Analytics />
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,19 @@
 "use client"
-import Image from "next/image";
-import { useRouter } from 'next/navigation';
 import {
   Card,
   CardContent,
-  CardDescription,
-  CardFooter,
   CardHeader,
-  CardTitle,
+  CardTitle
 } from "@/components/ui/card";
-import { Key } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
   const router = useRouter();
-  
+
   return (
     <>
-      <div>
-        <Image
-          className="-z-10 blur-"
-          src={"/background.svg"}
-          alt="Background image"
-          style={{ objectFit: "cover" }}
-          fill={true}
-        />
-      </div>
       <Card className="w-80 mx-auto mt-16 shadow-glass backdrop-blur-[10px]  border-1 bg-black/50">
         <CardHeader>
           <CardTitle className="text-white">Hey, I&apos;m Bytemax</CardTitle>
@@ -33,30 +22,29 @@ export default function Home() {
           <p>and I&apos;m having fun writing code.</p>
         </CardContent>
       </Card>
-      <Card className="w-80 mx-auto mt-40 shadow-glass backdrop-blur-[10px]  border-1 bg-black/50">
+      <Card className="w-80 mx-auto mt-40 shadow-glass backdrop-blur-[10px] border-1 bg-black/50">
         <CardHeader>
           <CardTitle className="text-white">My work</CardTitle>
         </CardHeader>
         <CardContent>
-         <Card
-            onClick={() => router.push('projects/dare_to_share')}
-            className="shadow-glass backdrop-blur-[10px]  border-1 bg-black/50 inline-flex "
-          >
-            <CardContent >
-              <Image
-                className="mt-5 rounded-md"
-                src={"/dare_to_share_icon.png"}
-                alt="dare to share icon"
-                width={50}
-                height={50}
-              >
-              </Image>
-            </CardContent>
-            <CardContent className="mt-8">
-              <span className="text-white">
-                Dare to Share
-              </span>
-            </CardContent>
+          <Card className="shadow-glass backdrop-blur-[10px] border-1 bg-black/50">
+            <Link className="flex" href={'projects/dare_to_share'}>
+              <CardContent className="m-auto">
+                <Image
+                  className="mt-5 rounded-md"
+                  src={"/dare_to_share_icon.png"}
+                  alt="dare to share icon"
+                  width={50}
+                  height={50}
+                >
+                </Image>
+              </CardContent>
+              <CardContent className="pt-8">
+                <span className="text-white">
+                  Dare to Share
+                </span>
+              </CardContent>
+            </Link>
           </Card>
         </CardContent>
       </Card>
